bfa00a2f1097bc90df541bd89f24ee21e79115d5
segun-ade <80776153+segun-ade@users.noreply.github.com>
2026-03-21 17:47:52 +0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -44,13 +44,13 @@ class CarModel(models.Model):  # Many-to-One relationship
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-                               validators = [
+                               validators=[
                                    MaxValueValidator(2023),
                                    MinValueValidator(2015)
                                ])
     # Other fields as needed
     price = models.IntegerField(default=10000,
-                                validators = [
+                                validators=[
                                     MaxValueValidator(5000),
                                     MinValueValidator(15000)
                                 ])
